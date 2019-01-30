a612b6e1a22ef768d2a085a6c3def5afd63fd5ff
ios <528392911@qq.com>
2019-01-30 10:37:01 +0800

    Initial commit

---
+++ Example/ModuleB/NSViewController.h
@@ -0,0 +1,13 @@
+//
+//  NSViewController.h
+//  ModuleB
+//
+//  Created by NShunJian on 01/30/2019.
+//  Copyright (c) 2019 NShunJian. All rights reserved.
+//
+
+@import UIKit;
+
+@interface NSViewController : UIViewController
+
+@end
